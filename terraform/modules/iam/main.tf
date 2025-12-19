c010c4fcc9ae5e78874e5741b0d33035d82c0a3c
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-12-19 23:16:09 +0530

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,13 +1,16 @@
-resource "aws_iam_user" "eks_ecr_user" {
-  name = "eks-ecr-evaluator"
+
+
+resource "aws_iam_user_policy_attachment" "eks_cluster" {
+  user       = aws_iam_user.eks_ecr_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_user_policy_attachment" "eks" {
+resource "aws_iam_user_policy_attachment" "eks_service" {
   user       = aws_iam_user.eks_ecr_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
 
 resource "aws_iam_user_policy_attachment" "ecr" {
   user       = aws_iam_user.eks_ecr_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
