4f55b94d4fc827c1e3003ecfe27117a5fef31129
Your Name <YourName@gmail.com>
2025-12-19 23:17:49 +0530

    Fix IAM policy: Replace missing AmazonEKSFullAccess with custom policy

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -2,9 +2,26 @@ resource "aws_iam_user" "eks_ecr_user" {
   name = "eks-ecr-evaluator"
 }
 
+resource "aws_iam_policy" "eks_full_access" {
+  name        = "eks-full-access"
+  path        = "/"
+  description = "Provides full access to Amazon EKS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "eks:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_user_policy_attachment" "eks" {
   user       = aws_iam_user.eks_ecr_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFullAccess"
+  policy_arn = aws_iam_policy.eks_full_access.arn
 }
 
 resource "aws_iam_user_policy_attachment" "ecr" {
