78bcadc49fcd59e870f1404ee37b7ce877b6a8fc
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-12-19 18:23:56 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -11,7 +11,7 @@ module "eks" {
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
-  #    manage_aws_auth_configmap = true
+  #    manage_aws_auth_configmap = true  
 
   eks_managed_node_groups = {
     default = {
