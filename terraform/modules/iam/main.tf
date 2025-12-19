8f371730b281b547b02619fb4799d795219393d6
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-12-19 23:20:02 +0530

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,16 +1,32 @@
 
 
-resource "aws_iam_user_policy_attachment" "eks_cluster" {
-  user       = aws_iam_user.eks_ecr_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_user" "eks_ecr_user" {
+  name = "eks-ecr-evaluator"
 }
 
-resource "aws_iam_user_policy_attachment" "eks_service" {
+resource "aws_iam_policy" "eks_full_access" {
+  name        = "eks-full-access"
+  path        = "/"
+  description = "Provides full access to Amazon EKS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "eks:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "eks" {
   user       = aws_iam_user.eks_ecr_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  policy_arn = aws_iam_policy.eks_full_access.arn
 }
 
 resource "aws_iam_user_policy_attachment" "ecr" {
   user       = aws_iam_user.eks_ecr_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
